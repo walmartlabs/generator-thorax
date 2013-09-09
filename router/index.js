51b43eeac3a7f13c7514390bb0e2b46f96442f98
Ryan Eastridge <ryan@eastridge.me>
2013-09-08 17:28:22 -0700

    Remove routes.json reference

--- router/index.js
+++ router/index.js
@@ -16,11 +16,3 @@ RouterGenerator.prototype.createRouter = function () {
   this._renderTemplate('js/routers');
 };
 
-RouterGenerator.prototype.appendRoute = function () {
-  var routes = JSON.parse(this.readFileAsString('routes.json'));
-  var router = (routes[this._.dasherize(this.name)] = {});
-
-  router[this._.slugify(this.name)] = 'index';
-
-  fs.writeFileSync('routes.json', JSON.stringify(routes, null, 2));
-};
