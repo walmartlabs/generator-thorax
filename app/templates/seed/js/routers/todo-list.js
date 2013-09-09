ca0249290d4d233804a53ee08dd386f0286230a5
Ryan Eastridge <ryan@eastridge.me>
2013-09-08 17:47:51 -0700

    Add todo list example files

---
+++ app/templates/seed/js/routers/todo-list.js
@@ -0,0 +1,22 @@
+define([
+  'backbone',
+  'collection',
+  'views/root',
+  'views/todo-list/index'
+], function(Backbone, Collection, RootView, TodoListIndexView) {
+  return Backbone.Router.extend({
+    routes: {
+      "todos": "index"
+    },
+    index: function() {
+      var collection = new Collection([{
+        title: 'First Todo',
+        done: true
+      }]);
+      var view = new TodoListIndexView({
+        collection: collection
+      });
+      RootView.getInstance().setView(view);
+    }
+  });
+}); 
---
+++ app/templates/seed/js/views/todo-list/index.js
@@ -0,0 +1,22 @@
+define([
+  'view',
+  'templates/todo-list/index'
+], function(View, template) {
+  return View.extend({
+    name: "todo-list/index",
+    template: template,
+    events: {
+      "submit form": function(event) {
+        event.preventDefault();
+        var attrs = this.serialize();
+        this.collection.add(attrs);
+        this.$('input[name="title"]').val('');
+      },
+      'change input[type="checkbox"]': function(event) {
+        var model = $(event.target).model();
+        model.set({done: event.target.checked});
+      }
+    }
+  });
+});
+
