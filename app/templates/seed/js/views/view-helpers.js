02bba15c6be90da0305d96f374ed82c3d4c7ddcd
Adam Krebs <amk528@cs.nyu.edu>
2013-10-18 01:48:26 -0400

    pass in local Handlebars instead of relying on the global

--- app/templates/seed/js/views/view-helpers.js
+++ app/templates/seed/js/views/view-helpers.js
@@ -1,4 +1,4 @@
-define(['handlebars'], function () {
+define(['handlebars'], function (Handlebars) {
   // Register any helpers you want available to Handlebars
   // Handlebars.registerViewHelper('helper', function () {
   //
