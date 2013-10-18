fda54d2a1bdd0c791b483af23054eeb6a672e286
Adam Krebs <amk528@cs.nyu.edu>
2013-10-17 21:42:25 -0400

    add stubbed-out methods to view, collection, router, and model

--- templates/_router.js
+++ templates/_router.js
@@ -4,7 +4,28 @@ define([
 ], function (RootView, Backbone) {
   return Backbone.Router.extend({
     routes: {
+      '': 'posts',
+      'posts': 'posts'
+      'posts/:id': 'posts'
+    },
 
+    initialize: function() {
+      // set up any instance variables here
+      // this.posts = new PostsCollection;
+    },
+
+    posts: function(id) {
+      if (id) {
+        // Fetch a single model with id `id` and render a view for it
+        var model = this.posts.get(id);
+        var view = new PostView({model: model});
+        model.fetch();
+      } else {
+        // Fetch and populate the posts collection and render a collection view for it
+        var view = new PostsView({collection: this.posts});
+        this.posts.fetch();
+      }
     }
+
   });
 });
--- templates/_view.js
+++ templates/_view.js
@@ -4,6 +4,21 @@ define([
 ], function (View, template) {
   return View.extend({
     name: '<%= name %>',
-    template: template
+    template: template,
+
+    events: {
+
+    },
+
+    // set up any child views either in initialize or directly on the prototype
+    child: new View(),
+
+    // Set up any event listeners on the view's model / collection and any
+    // child views
+    initialize: function(options) {
+      this.listenTo(this.model, 'change', this.render);
+      this.child.on('invalid', this.markAsInvalid, this);
+    }
+
   });
 });
