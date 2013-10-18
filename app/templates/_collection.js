d0ed26ad69b4bef69065d90e99040031fa362fc0
Adam Krebs <amk528@cs.nyu.edu>
2013-10-17 21:56:45 -0400

    add notes about base modules

--- app/templates/_collection.js
+++ app/templates/_collection.js
@@ -1,4 +1,10 @@
 define(['thorax'], function (Thorax) {
+  // This is your base Collection class. All Collections in your app should inherit from
+  // this module, so feel free to define any app-wide specifics here.
+  // (i.e. override `url` to combine your API root with a collection-specific `urlPath`
+  // or `name` paramter that Backbone can use, or automatically extract nested
+  // JSON using a `name` parameter)
+
   return Thorax.Collection.extend({
 
   });
