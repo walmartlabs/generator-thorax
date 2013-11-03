157c3a1e935366f47a41ef412d2a727486b15c9e
Glen Baker <iepathos@gmail.com>
2013-11-03 03:13:01 -0800

    Fixed Gruntfile template so that it will properly put quotes around the styleProcessor like less

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -318,7 +318,7 @@ module.exports = function(grunt) {
   grunt.registerTask('styles', [
     'copy:styles'<% if (includeBootstrap) { %>,
     'copy:bootstrap'<% } %><% if (styleProcessor !== 'none') { %>,
-    <%= styleProcessor %><% } %>
+    '<%= styleProcessor %>'<% } %>
   ]);
 
   grunt.registerTask('default', [
