3b0002e4c5c8e5ca785703d1883f77c8af5835d5
Erik Trom <erik.trom@gmail.com>
2013-12-03 17:39:32 -0800

    simplify logic in main.js
    closes #98

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -7,35 +7,23 @@ require([<% if (useZepto) { %>
   '<% if (includeCoffeeScript) { %>cs!<% } %>routers/todo-list',<% } %>
   '<% if (includeCoffeeScript) { %>cs!<% } %>helpers'
 ], function ($, Backbone, RootView<% if (starterApp === 'Hello World') { %>, HelloWorldRouter<% } else if (starterApp === 'Todo List') { %>, TodoListRouter <% } %>) {
+  $(function() {
+    Backbone.history.start({
+      pushState: false,
+      root: '/',
+      silent: true
+    });
 
-  initialize(function(next) {
-    // Load any data that your app requires to boot
-    // and initialize all routers here, the callback
-    // `next` is provided in case the operations
-    // needed are aysynchronous
+    // RootView may use link or url helpers which
+    // depend on Backbone history being setup
+    // so need to wait to loadUrl() (which will)
+    // actually execute the route
+    RootView.getInstance(document.body);
+
+    // Initialize your routers here
     <% if (starterApp === 'Hello World') { %>new HelloWorldRouter();<% } else if (starterApp === 'Todo List') { %>new TodoListRouter();<% } %>
 
-    next();
+    // This will trigger your routers to start
+    Backbone.history.loadUrl();
   });
-
-  function initialize(complete) {
-    $(function() {
-      Backbone.history.start({
-        pushState: false,
-        root: '/',
-        silent: true
-      });
-
-      // RootView may use link or url helpers which
-      // depend on Backbone history being setup
-      // so need to wait to loadUrl() (which will)
-      // actually execute the route
-      RootView.getInstance(document.body);
-
-      complete(function() {
-        Backbone.history.loadUrl();
-      });
-    });
-  }
-
 });
