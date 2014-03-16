6735540f48c54e08451264722ae12e87b800c444
Erik Trom <erik.trom@gmail.com>
2014-03-16 15:09:14 -0700

    remove registerViewHelper and specs for coffee, js and require statement for test-setup-browser

--- app/templates/seed/js/helpers.js
+++ app/templates/seed/js/helpers.js
@@ -1,4 +1,4 @@
-define(['handlebars', 'handlebones'], function (Handlebars) {
+define(['handlebars'], function (Handlebars) {
 
   /**
    * Register a normal handlebars helper
@@ -10,38 +10,5 @@ define(['handlebars', 'handlebones'], function (Handlebars) {
    */
   Handlebars.registerHelper('greeting', function () {
     return new Handlebars.SafeString('Hello World');
-  });
-
-  /**
-   * Register a new block helper that will create and embed a HelperView
-   * instance with its template set to the captured block.
-   * A helper view is different than a child view. It's context will be that of
-   * template in which it is placed.
-   *
-   * Example usage:
-   *
-   * In template file:
-   *
-   * {{#on "incremented"}}{{i}}{{/on}}
-   * {{#button trigger="incremented"}}Add again{{/button}}
-   *
-   * In class of template:
-   *
-   * events: {
-   *   incremented: function() {
-   *     ++this.i;
-   *   }
-   * },
-   * initialize: function() {
-   *   this.i = 0;
-   * }
-   *
-   * Checkout out test/helpers/view-helpers.spec an example with tests
-   *
-   */
-  Handlebars.registerViewHelper('on', function (eventName, helperView) {
-    helperView.parent.on(eventName, function() {
-      helperView.render();
-    });
   });
 });
